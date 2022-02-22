fb097cb48e377da7722abdc6de96a781069d4622
Reinaldo Paixao <reipaixao.b@gmail.com>
2022-02-22 14:20:53 -0300

    Primeiras Configs

---
+++ models/user.js
@@ -0,0 +1,12 @@
+// Aula 24.1 - 1:10
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  });
+ 
+  return User;
+};
